18091308355e1c1b21ef3fec54c437e7bea1c2d0
spranav <sumit.pranav@gmail.com>
2022-05-06 23:18:14 +0530

    updated layout, home and created empty pages

---
+++ narayaniaesthetics.com.web/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace narayaniaesthetics.com.web.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
